80c60997a116f3f84e502a0fabaca2d32618ea33
Zurceel <gabrielcruzsil@gmail.com>
2022-05-26 10:20:16 -0300

    ajustando o erro de subir imagem no cosmic

--- services/uploadImagemCosmic.ts
+++ services/uploadImagemCosmic.ts
@@ -1,38 +1,39 @@
 import multer from "multer";
-import cosmicjs from "cosmicjs"
+import cosmicjs from "cosmicjs";
 
-const {CHAVE_GRAVACAO_DOS_AVATARES,
-    CHAVE_GRAVACAO_DOS_PUBLICACOES,
+const {
+    CHAVE_GRAVACAO_AVATARES,
+    CHAVE_GRAVACAO_PUBLICACOES,
     BUCKET_AVATARES,
-    BUCKET_PUBLICACOES} = process.env 
+    BUCKET_PUBLICACOES} = process.env;
 
-const Cosmic = cosmicjs()
+const Cosmic = cosmicjs();
 const bucketAvatares = Cosmic.bucket({
     slug: BUCKET_AVATARES,
-    write_hey: CHAVE_GRAVACAO_DOS_AVATARES
-})
+    write_key: CHAVE_GRAVACAO_AVATARES
+});
 
 const bucketPublicacoes = Cosmic.bucket({
     slug: BUCKET_PUBLICACOES,
-    write_hey: CHAVE_GRAVACAO_DOS_PUBLICACOES
-})
+    write_key: CHAVE_GRAVACAO_PUBLICACOES
+});
 
-const storage = multer.memoryStorage()
-const updload = multer({storage: storage})
+const storage = multer.memoryStorage();
+const updload = multer({storage : storage});
 
-const uploadImagemCosmic = async(req: any) => {
-    if(req?.file?.orgiginalname){
-        const media_objetc = {
-            orgiginalname: req.file.orgiginalname,
-            Buffer: req.file.Buffer
-        }
+const uploadImagemCosmic = async(req : any) => {
+    if(req?.file?.originalname){
+        const media_object = {
+            originalname: req.file.originalname,
+            buffer : req.file.buffer
+        };
 
         if(req.url && req.url.includes('publicacao')){
-            return await bucketPublicacoes.addMedia({media: media_objetc})
+            return await bucketPublicacoes.addMedia({media : media_object});
         }else{
-            return await bucketAvatares.addMedia({media: media_objetc})
+            return await bucketAvatares.addMedia({media : media_object});
         }
     }
 }
 
-export {updload, uploadImagemCosmic}
+export {updload, uploadImagemCosmic};
