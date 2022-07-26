c3521c5c7cdd2868cd78d55d8367d0b3270813e7
Zurceel <gabrielcruzsil@gmail.com>
2022-07-26 09:58:11 -0300

    tentando corrigir o erro na publicação.

--- services/uploadImagemCosmic.ts
+++ services/uploadImagemCosmic.ts
@@ -23,12 +23,19 @@ const updload = multer({storage : storage});
 
 const uploadImagemCosmic = async(req : any) => {
     if(req?.file?.originalname){
+
+        if(!req.file.originalname.includes('.png') &&
+            !req.file.originalname.includes('.jpg') && 
+            !req.file.originalname.includes('.jpeg')){
+                throw new Error('Extensao da imagem invalida');
+        } 
+
         const media_object = {
             originalname: req.file.originalname,
             buffer : req.file.buffer
         };
 
-        if(req.url && req.url.includes('publicacoes')){
+        if(req.url && req.url.includes('publicacao')){
             return await bucketPublicacoes.addMedia({media : media_object});
         }else{
             return await bucketAvatares.addMedia({media : media_object});
